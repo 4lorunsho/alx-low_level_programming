a268bb9b68067ea344af258518ad807266dde830
4lorunsho <allifolorunsho@gmail.com>
2022-09-15 13:50:58 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * * main - entry point 
- *  Description: print all single digit numbers 
+ * Description: print all single digit numbers 
  *	* Return: Always 0 (Success)
  */
 int main(void)
