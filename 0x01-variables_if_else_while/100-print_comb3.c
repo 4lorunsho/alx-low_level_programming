a4c4b95b6238614b0f6022808b1176d4f94b7ab6
4lorunsho <allifolorunsho@gmail.com>
2022-09-19 17:07:01 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e == 56)
+			{
+				putchar(e);
+				purchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
