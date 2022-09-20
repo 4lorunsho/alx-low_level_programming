ef59215ff1d77920fd7c0af8440e939485dd49eb
4lorunsho <allifolorunsho@gmail.com>
2022-09-20 04:52:25 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,17 +2,19 @@
 #include <string.h>
 
 /**
- * strlen - a function that returns the length of a string
+ * _strlen - a function that returns the length of a string
  * @s: char input
  * Return: length of the input string
  */
-int strlen(char *s)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i = 1, sum = 0;
-	char pl = s[0];
-	{
-		sum++;
-		pl = s[i++];
-	}
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (sum);
 }
